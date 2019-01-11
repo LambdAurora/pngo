a6830ab5222a7c5c1dfa9e8ae9f289c4120a3a41
LambdAurora <aurora42lambda@gmail.com>
2019-01-11 15:40:55 +0100

    :sparkles: Added button handling.

--- garage_velo.ino
+++ garage_velo.ino
@@ -5,10 +5,15 @@
 
 #define MAX_USERS 4
 #define MAX_PLOTS 4
+#define MAX_WAIT 300
 
-#define RED_PIN 2
-#define GREEN_PIN 3
-#define BLUE_PIN 4
+#define BTN_ADD 25
+#define BTN_RESET_WAIT 100
+
+#define BTN_PIN 2
+#define RED_PIN 3
+#define GREEN_PIN 5
+#define BLUE_PIN 6
 
 size_t data_current_address = 0;
 RFID_ID users[MAX_USERS] = { {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0} };
@@ -24,56 +29,6 @@ struct Storage
 typedef struct Storage Storage;
 
 /*!
- * Definition of a plot.
- */
-struct Plot
-{
-  bool valid = false;
-  uint16_t lock_pin;
-  uint16_t indicator_pin;
-  int user;
-};
-
-Plot plots[4];
-
-/*!
- * Creates a plot instance.
- */
-Plot make_plot(uint16_t lock_pin, uint64_t indicator_pin, int user)
-{
-  Plot plot{};
-  plot.valid = true;
-  plot.lock_pin = lock_pin;
-  plot.indicator_pin = indicator_pin;
-  plot.user = user;
-  return plot; 
-}
-
-bool Plot_has_user(const Plot &plot)
-{
-  return plot.user > -1;
-}
-
-int get_plot_from_user(size_t user)
-{
-  for (size_t i = 0; i < MAX_PLOTS; i++)
-    if (plots[i].user == user) return i;
-  return -1;
-}
-
-int get_first_empty_plot()
-{
-  for (size_t i = 0; i < MAX_PLOTS; i++)
-    if (!Plot_has_user(plots[i])) return i; 
-  return -1;
-}
-
-bool has_empty_plot()
-{
-  return get_first_empty_plot() != -1;
-}
-
-/*!
  * Initializes the storage.
  */
 void init_storage()
@@ -84,14 +39,6 @@ void init_storage()
     // Write defaults.
     EEPROM.write(addr, 0x42);
     addr++;
-    for (size_t i = 0; i < MAX_PLOTS; i++) {
-      EEPROM.write(addr, 0);
-      addr++;
-      EEPROM.write(addr, 0);
-      addr++;
-      EEPROM.write(addr, -1);
-      addr++;
-    }
     Storage storage{};
     EEPROM.put(addr, storage);
   }
@@ -108,16 +55,6 @@ void read_storage()
   if (EEPROM.read(addr) == 0x42) {
     Storage storage;
     addr++;
-    for (size_t i = 0; i < MAX_PLOTS; i++) {
-      uint16_t lock_pin = EEPROM.read(addr);
-      addr++;
-      uint16_t indicator_pin = EEPROM.read(addr);
-      addr++;
-      int user = EEPROM.read(addr);
-      plots[i] = make_plot(lock_pin, indicator_pin, user);
-      Serial.println("Loaded plot #" + String(i) + "...");
-      addr++;
-    }
     EEPROM.get(addr, storage);
     users[0] = {storage.u0_0, storage.u0_1, storage.u0_2, storage.u0_3};
     users[1] = {storage.u1_0, storage.u1_1, storage.u1_2, storage.u1_3};
@@ -135,16 +72,6 @@ void write_storage()
   size_t addr = 0x00;
   EEPROM.write(addr, 0x42);
   addr++;
-  for (size_t i = 0; i < MAX_PLOTS; i++) {
-    Plot plot = plots[i];
-    EEPROM.write(addr, plot.lock_pin);
-    addr++;
-    EEPROM.write(addr, plot.indicator_pin);
-    addr++;
-    EEPROM.write(addr, plot.user);
-    Serial.println("Saved plot #" + String(i) + "...");
-    addr++;
-  }
   Storage storage{};
   // User #0
   storage.u0_0 = users[0].get_bit_0();
@@ -169,19 +96,37 @@ void write_storage()
   EEPROM.put(addr, storage);
 }
 
+bool has_user(const RFID_ID &id)
+{
+  for (size_t i = 0; i < MAX_USERS; i++)
+    if (users[i] == id)
+      return true;
+  return false;
+}
+
+size_t get_first_null_user()
+{
+  for (size_t i = 0; i < MAX_USERS; i++) {
+    if (users[i] == RFID_ID(0, 0, 0, 0))
+      return i;
+  }
+  return MAX_USERS;
+}
+
 /*
  * LEDS
  */
 
 const Color RED{1.f, 0.f, 0.f};
 const Color GREEN{0.f, 1.f, 0.f};
-const Color YELLOW{1.f, .50f, 0.f};
+const Color BLUE{0.f, 0.f, 1.f};
+const Color YELLOW{.50f, .40f, 0.f};
 
 void display_color(const Color &color)
 {
-  analogWrite(RED_PIN, color.red() * 255);
-  analogWrite(GREEN_PIN, color.green() * 255);
-  analogWrite(BLUE_PIN, color.blue() * 255); 
+  analogWrite(RED_PIN, pow(color.red(), 2.f) * 255);
+  analogWrite(GREEN_PIN, pow(color.green(), 2.f) * 255);
+  analogWrite(BLUE_PIN, pow(color.blue(), 2.f) * 255); 
 }
 
 void clear_color_led()
@@ -197,6 +142,9 @@ void clear_color_led()
 
 RFID rfid_card(7, 8);
 RFID_ID test_id(0x98, 0xC0, 0x6D, 0xDE);
+uint8_t state(0);
+uint16_t wait_count(0);
+uint16_t btn_repeat(0);
 
 /*!
  * Setups the entire program.
@@ -208,6 +156,7 @@ void setup()
 
   init_storage();
 
+  pinMode(BTN_PIN, INPUT);
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
@@ -224,6 +173,13 @@ void setup()
 void loop() 
 {
   rfid_card.update();
+
+  handle_button();
+
+  if (state == 1) {
+    on_add();
+    return;
+  }
 
   if (!rfid_card.has_card_present()) {
     clear_color_led();
@@ -245,29 +201,21 @@ void loop()
     delay(100);
     return;
   }
-
-  auto plot = get_plot_from_user(user);
-  if (plot < 0) {
-    // Allocate a plot for the user.
-  } else {
-    // Free the user's plot.
-  }
   
   Serial.print("Has card: ");
   Serial.print(rfid_card.has_card_present());
   if (rfid_card.has_new_card()) {
+    bool success = false;
     for (size_t i = 0; i < MAX_USERS; i++) {
       if (users[i] == rfid_card.get_current_id()) {
+        display_color(GREEN);
         Serial.println(" Hello user #" + String(i));
+        success = true;
+        break;
       }
     }
-    if (rfid_card.get_current_id() == test_id) {
-      Serial.println(" Hello Jean-Pierre!");
-      display_color(GREEN);
-    } else {
-      Serial.println(" Go away!");
+    if (!success)
       display_color(RED);
-    }
   } 
   if (rfid_card.has_card_present()) {
     Serial.print(" New: ");
@@ -277,4 +225,88 @@ void loop()
     Serial.println();
   }
   delay(100);
+}
+
+void on_add()
+{
+  if (rfid_card.has_card_present()) {
+    auto i = get_first_null_user();
+    auto id = rfid_card.get_current_id();
+    
+    // Cannot add an user because all slots are filled.
+    if (i == MAX_USERS) {
+      Serial.println("[P'n'Go][Error] Cannot add new user: all slots are filled.");
+      display_color(RED);
+      delay(500);
+      display_color(BLUE);
+      delay(500);
+      display_color(BLUE);
+      delay(500);
+      clear_color_led();
+      state = 0;
+      return;
+    } else if (has_user(id)) {
+      Serial.println("[P'n'Go][Error] Cannot add new user: user already added.");
+      display_color(GREEN);
+      delay(500);
+      display_color(RED);
+      delay(500);
+      display_color(BLUE);
+      delay(500);
+      clear_color_led();
+      state = 0;
+      return;
+    }
+
+    users[i] = id;
+    Serial.println("[P'n'Go] Added user '" + id.to_string() + "'.");
+    wait_count = 0;
+    state = 0;
+  } else {
+    if (wait_count >= MAX_WAIT) {
+      display_color(BLUE);
+      delay(500);
+      clear_color_led();
+      delay(500);
+      display_color(BLUE);
+      delay(500);
+      display_color(RED);
+      delay(500);
+      clear_color_led();
+      wait_count = 0;
+      state = 0;
+    }
+  }
+}
+
+void reset()
+{
+  for (size_t i = 0; i < MAX_USERS; i++)
+    users[i] = RFID_ID(0, 0, 0, 0);
+  users[0] = RFID_ID(0,0,0,0);
+  write_storage();
+}
+
+void handle_button()
+{
+  auto btn_value = digitalRead(BTN_PIN);
+  if (btn_value == LOW) {
+    if (btn_repeat > BTN_RESET_WAIT) {}
+    else if (btn_repeat > BTN_ADD) {
+      state = 1;
+    }
+    btn_repeat = 0;
+  } else if (btn_value == HIGH) {
+    if (btn_repeat == BTN_ADD) Serial.println("btn_helper: Ready to add user.");
+    btn_repeat++;
+    if (btn_repeat == BTN_RESET_WAIT + 1) {
+      Serial.println("[P'n'Go][WARNING] Resetting...");
+      reset();
+      display_color(GREEN);
+      delay(10);
+      display_color(RED);
+      delay(10);
+      clear_color_led();
+    }
+  }
 }
